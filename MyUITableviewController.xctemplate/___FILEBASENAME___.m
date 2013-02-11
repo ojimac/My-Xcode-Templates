ef8c46ba97d6e3f9fcbeeb8788801f18f04c35f2
ojimac <ojimac@gmail.com>
2013-02-11 21:48:57 +0900

    added MyUITableViewController Template

---
+++ MyUITableviewController.xctemplate/___FILEBASENAME___.m
@@ -0,0 +1,63 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import "___FILEBASENAME___.h"
+
+@interface ___FILEBASENAMEASIDENTIFIER___()
+
+@end
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+- (id)initWithStyle:(UITableViewStyle)style {
+    self = [super initWithStyle:style];
+
+    if (self) {
+    }
+
+    return self;
+}
+
+#pragma mark -
+#pragma mark View Lifecycle
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+#pragma mark -
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+
+    // Configure the cell...
+
+    return cell;
+}
+
+#pragma mark -
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+}
+
+@end
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-This Repository is My Xcode Custom Templates.
-=========
+## This Repository is My Xcode Custom Templates.
+
+## Usage
+
+```shell
+$ cp -r MyTemplate /Library/Developer/Xcode/Templates/Application/File\ Templates/
+```
